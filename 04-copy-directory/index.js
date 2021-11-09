a054db3f7a146fd70403389a6843b473ee5da119
heinrich-marx <weriast98@gmail.com>
2021-11-10 00:23:58 +0300

    fix: fix 4 task(10.11.21 00:23)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -37,26 +37,21 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
   if (err) throw err;
   fs.access("04-copy-directory/files-copy", (err) => {
     if (!err) {
-      fs.rmdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
+      fs.readdir("04-copy-directory/files-copy", (err, files) => {
         if (err) throw err;
-        fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
-          if (err) throw err;
-        });
-        const dir2 = path.join(__dirname, "files-copy");
-        files.forEach((el) => {
-          fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
+        //  console.log(files);
+        for (let el of files) {
+          fs.unlink(path.join("04-copy-directory/files-copy", el), (err) => {
             if (err) throw err;
-            else {
-              fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
-                if (err) throw err;
-                //   else console.log("ok");
-              });
-            }
           });
-        });
+        }
       });
+      // fs.rmdir("04-copy-directory/files-copy", (err) => {
+      //   if (err) throw err;
+      // });
     }
   });
+
   fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
     if (err) throw err;
   });
