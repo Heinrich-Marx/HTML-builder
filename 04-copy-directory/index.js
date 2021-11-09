5cfd15a0c866065d5fee4027a08b4a0cdfbea45d
heinrich-marx <weriast98@gmail.com>
2021-11-09 23:54:22 +0300

    fix: fix 4 task also(9.11.21 23:54)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -39,38 +39,37 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
     if (!err) {
       fs.rmdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
         if (err) throw err;
-      });
-    } else {
-      fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
-        if (err) throw err;
-      });
-      const dir2 = path.join(__dirname, "files-copy");
-      files.forEach((el) => {
-        fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
+        fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
           if (err) throw err;
-          else {
-            fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
-              if (err) throw err;
-              //   else console.log("ok");
-            });
-          }
+        });
+        const dir2 = path.join(__dirname, "files-copy");
+        files.forEach((el) => {
+          fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
+            if (err) throw err;
+            else {
+              fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
+                if (err) throw err;
+                //   else console.log("ok");
+              });
+            }
+          });
         });
       });
     }
   });
-  // fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
-  //   if (err) throw err;
-  // });
-  // const dir2 = path.join(__dirname, "files-copy");
-  // files.forEach((el) => {
-  //   fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
-  //     if (err) throw err;
-  //     else {
-  //       fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
-  //         if (err) throw err;
-  //         //   else console.log("ok");
-  //       });
-  //     }
-  //   });
-  // });
+  fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  const dir2 = path.join(__dirname, "files-copy");
+  files.forEach((el) => {
+    fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
+      if (err) throw err;
+      else {
+        fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
+          if (err) throw err;
+          //   else console.log("ok");
+        });
+      }
+    });
+  });
 });
