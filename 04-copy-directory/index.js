e7d34c9910912196a608ce6d17ad356a9deb70d1
heinrich-marx <weriast98@gmail.com>
2021-11-10 12:21:39 +0300

    fix:fix bug with copy images(10.11.21 12:21)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,35 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-// const dir = path.join(__dirname, "files");
-// fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-//   if (err) throw err;
-//   fs.access('04-copy-directory/files-copy',(err) => {
-//     if (err) {
-//       const dir2 = path.join(__dirname, "files-copy");
-//       files.forEach((el) => {
-//         fs.readFile(`${dir}/${el.name}`, "utf-8", (err, data) => {
-//           if (err) throw err;
-//           else {
-//             fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
-//               if (err) throw err;
-//               //   else console.log("ok");
-//             });
-//           }
-//         });
-//       });
-//     } else {
-//       fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
-//         if (err) throw err;
-//       });
-//       fs.readdir('04-copy-directory/files-copy', (err, data) => {
-//         if (err) throw err;
-
-//       });
-//     }
-//   });
-
-// });
-
 const fs = require("fs");
 const path = require("path");
 const dir = path.join(__dirname, "files");
@@ -46,9 +14,6 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
           });
         }
       });
-      // fs.rmdir("04-copy-directory/files-copy", (err) => {
-      //   if (err) throw err;
-      // });
     }
   });
 
@@ -62,7 +27,6 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
       else {
         fs.writeFile(`${dir2}/${el.name}`, data, "utf-8", (err) => {
           if (err) throw err;
-          //   else console.log("ok");
         });
       }
     });
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -102,7 +102,6 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
     if (err) throw err;
   });
   const dir2 = path.join(__dirname, "project-dist/assets");
-  // console.log(files)
   files.forEach((el) => {
     recursive(el);
     function recursive(value) {
@@ -110,14 +109,16 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
         fs.readFile(`${dir}/${el.name}/${value.name}`, "utf-8", (err, data) => {
           if (err) throw err;
           else {
-            fs.writeFile(
-              `${dir2}/${el.name}/${value.name}`,
-              data,
-              "utf-8",
-              (err) => {
-                if (err) throw err;
-                // else console.log("ok");
-              }
+            // fs.writeFile(
+            //   `${dir2}/${el.name}/${value.name}`,
+            //   data,
+            //   "utf-8",
+            //   (err) => {
+            //     if (err) throw err;
+            //   }
+            // );
+            fs.createReadStream(`${dir}/${el.name}/${value.name}`).pipe(
+              fs.createWriteStream(`${dir2}/${el.name}/${value.name}`)
             );
           }
         });
@@ -146,51 +147,73 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
   });
 });
 
-// fs.readdir(assets,'utf-8', (err,files) => {
-//     if (err) throw  err
-//     fs.mkdir('06-build-page/project-dist',{recursive: true},err=> {
-//         if (err) throw  err
+// // COPY FOLDER ASSETS
+// fs.access("06-build-page/project-dist/assets", err => {
+//   if (!err) {
+//     fs.readdir("06-build-page/project-dist/assets", (err,files) => {
+//       files.isFile() ? log
+//       if (err) throw  err;
+//       files.forEach((elem) => {
+//         recursiveDelete(elem)
+//         function recursiveDelete (value) {
+//           if (value.isFile()) {
+//             fs.unlink(path.join(`06-build-page/project-dist/assets/${elem.name}`, value.name), (err) => {
+//               if (err) throw  err;
+//             })
+//           } else {
+//             console.log(123)
+//           }
+//         }
+//       })
 //     })
-//     fs.mkdir('06-build-page/project-dist/assets',{recursive: true}, err => {
-//         if (err) throw err
-//     })
-//    // console.log(assets)
-//     const assets2 = path.join(__dirname, 'project-dist/assets')
-//     files.forEach((el) => {
-//         // fs.readFile(recurciveFile(el),'utf-8',(err,data) => {
-//         //     if (err) throw err
-//         //     console.log('ok')
-//         // })
-//         // console.log(recurciveFile(el))
-//         recurciveFile(el)
-//     })
+
+//   }
 // })
-//
-// function recurciveFile (name) {
-//     const name1 = path.join(__dirname,`assets/${name}`)
-//     //console.log(name1)
-//     fs.readdir(name1,'utf-8',(err,data) => {
-//         if (err) throw  err
-//
-//
-//
-//         // data.forEach((el) => {
-//         //     console.log(name1, `-`, el)
-//         // })
-//         console.log(name1, '-', data)
-//         // else {
-//         //     if (data.isFile()) {
-//         //         return path.dirname(data)
-//         //     } else {
-//         //         fs.readdir(data,{withFileTypes: true},(err,data2) => {
-//         //             if (err) throw  err
-//         //             data2.forEach((el) => {
-//         //                 if (el.isFile()) return path.dirname(el)
-//         //                 else recurciveFile(el)
-//         //             })
-//         //         })
-//         //     }
-//         // }
-//     })
-//
-// }
+
+// fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+//   if (err) throw err;
+//   fs.mkdir("06-build-page/project-dist/assets", { recursive: true }, (err) => {
+//     if (err) throw err;
+//   });
+//   const dir2 = path.join(__dirname, "project-dist/assets");
+//   files.forEach((el) => {
+//     recursive(el);
+//     function recursive(value) {
+//       if (value.isFile()) {
+//         fs.readFile(`${dir}/${el.name}/${value.name}`, "utf-8", (err, data) => {
+//           if (err) throw err;
+//           else {
+//             fs.writeFile(
+//                 `${dir2}/${el.name}/${value.name}`,
+//                 data,
+//                 "utf-8",
+//                 (err) => {
+//                   if (err) throw err;
+//                 }
+//             );
+//           }
+//         });
+//       } else {
+//         fs.mkdir(
+//             `06-build-page/project-dist/assets/${value.name}`,
+//             { recursive: true },
+//             (err) => {
+//               if (err) throw err;
+//             }
+//         );
+
+//         const dir3 = path.join(__dirname, `assets/${value.name}`);
+//         fs.readdir(dir3, { withFileTypes: true }, (err, data) => {
+//           if (err) throw err;
+//           const dir2 = path.join(
+//               __dirname,
+//               `project-dist/assets/${value.name}`
+//           );
+//           data.forEach((elem) => {
+//             recursive(elem);
+//           });
+//         });
+//       }
+//     }
+//   });
+// });
