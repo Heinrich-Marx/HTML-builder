cd63431f4f3a11f56e649d678b01cbce4fa1902c
heinrich-marx <weriast98@gmail.com>
2021-11-11 10:03:21 +0300

    fix: fix 6 task(11.11.21 10:03)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,78 +1,75 @@
 const fs = require("fs");
 const path = require("path");
-const dir = path.join(__dirname, "assets");
-const dirStyle = path.join(__dirname, "styles");
-fs.mkdir("06-build-page/project-dist", { recursive: true }, (err) => {
+const dir = path.resolve(__dirname, "assets");
+const dirStyle = path.resolve(__dirname, "styles");
+const proj = path.resolve(__dirname, "project-dist");
+const template = path.resolve(__dirname, "template.html");
+const header = path.resolve(__dirname, "components/header.html");
+const footer = path.resolve(__dirname, "components/footer.html");
+const article = path.resolve(__dirname, "components/articles.html");
+const about = path.resolve(__dirname, "components/about.html");
+// MAKE HTML FILE AND REPLACE TAGS
+fs.mkdir(proj, { recursive: true }, (err) => {
   if (err) throw err;
 });
 
-// MAKE HTML FILE AND REPLACE TAGS
-const html = fs.createReadStream("06-build-page/template.html", "utf-8");
-const newHtml = fs.createWriteStream("06-build-page/project-dist/index.html");
+// fs.open(way, "w", (err) => {
+//   if (err) throw err;
+// });
+
+const way = path.resolve(__dirname, "project-dist/index.html");
+// fs.writeFile(way, template, 'utf-8', (err) => {
+//   if (err) throw err;
+// });
+// console.log(way);
+const html = fs.createReadStream(template, "utf-8");
+const newHtml = fs.createWriteStream(way);
 html.pipe(newHtml);
-fs.readFile("06-build-page/project-dist/index.html", "utf-8", (err, data) => {
-  if (err) throw err;
-  fs.readFile("06-build-page/components/header.html", "utf-8", (err, data2) => {
+
+// // fs.open(way, "", (err) => {
+// //   if (err) throw err;
+// // });
+
+setTimeout(() => {
+  fs.readFile(way, "utf-8", (err, data) => {
     if (err) throw err;
-    data = data.replace("{{header}}", data2);
-    fs.writeFile(
-      "06-build-page/project-dist/index.html",
-      data,
-      "utf-8",
-      (err) => {
+    // console.log(data);
+
+    fs.readFile(header, "utf-8", (err, data2) => {
+      if (err) throw err;
+      data = data.replace("{{header}}", data2);
+      fs.writeFile(way, data, "utf-8", (err) => {
         if (err) throw err;
-      }
-    );
-  });
-  fs.readFile(
-    "06-build-page/components/articles.html",
-    "utf-8",
-    (err, data3) => {
+      });
+    });
+    fs.readFile(article, "utf-8", (err, data3) => {
       if (err) throw err;
       data = data.replace("{{articles}}", data3);
-      fs.writeFile(
-        "06-build-page/project-dist/index.html",
-        data,
-        "utf-8",
-        (err) => {
-          if (err) throw err;
-        }
-      );
-    }
-  );
-  fs.readFile("06-build-page/components/footer.html", "utf-8", (err, data4) => {
-    if (err) throw err;
-    data = data.replace("{{footer}}", data4);
-    fs.writeFile(
-      "06-build-page/project-dist/index.html",
-      data,
-      "utf-8",
-      (err) => {
+      fs.writeFile(way, data, "utf-8", (err) => {
         if (err) throw err;
-      }
-    );
-  });
-  fs.access("06-build-page/components/about.html", (err) => {
-    if (!err) {
-      fs.readFile(
-        "06-build-page/components/about.html",
-        "utf-8",
-        (err, data5) => {
+      });
+    });
+    fs.readFile(footer, "utf-8", (err, data4) => {
+      if (err) throw err;
+      data = data.replace("{{footer}}", data4);
+      fs.writeFile(way, data, "utf-8", (err) => {
+        if (err) throw err;
+      });
+    });
+
+    fs.access(about, (err) => {
+      if (!err) {
+        fs.readFile(about, "utf-8", (err, data5) => {
           if (err) throw err;
           data = data.replace("{{about}}", data5);
-          fs.writeFile(
-            "06-build-page/project-dist/index.html",
-            data,
-            "utf-8",
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      );
-    }
+          fs.writeFile(way, data, "utf-8", (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
   });
-});
+}, 10);
 
 // MAKE STYLE.CSS
 const style = path.join(__dirname, "project-dist/style.css");
@@ -147,73 +144,73 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => {
   });
 });
 
-// // COPY FOLDER ASSETS
-// fs.access("06-build-page/project-dist/assets", err => {
-//   if (!err) {
-//     fs.readdir("06-build-page/project-dist/assets", (err,files) => {
-//       files.isFile() ? log
-//       if (err) throw  err;
-//       files.forEach((elem) => {
-//         recursiveDelete(elem)
-//         function recursiveDelete (value) {
-//           if (value.isFile()) {
-//             fs.unlink(path.join(`06-build-page/project-dist/assets/${elem.name}`, value.name), (err) => {
-//               if (err) throw  err;
-//             })
-//           } else {
-//             console.log(123)
-//           }
-//         }
-//       })
-//     })
+// // // COPY FOLDER ASSETS
+// // fs.access("06-build-page/project-dist/assets", err => {
+// //   if (!err) {
+// //     fs.readdir("06-build-page/project-dist/assets", (err,files) => {
+// //       files.isFile() ? log
+// //       if (err) throw  err;
+// //       files.forEach((elem) => {
+// //         recursiveDelete(elem)
+// //         function recursiveDelete (value) {
+// //           if (value.isFile()) {
+// //             fs.unlink(path.join(`06-build-page/project-dist/assets/${elem.name}`, value.name), (err) => {
+// //               if (err) throw  err;
+// //             })
+// //           } else {
+// //             console.log(123)
+// //           }
+// //         }
+// //       })
+// //     })
 
-//   }
-// })
+// //   }
+// // })
 
-// fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-//   if (err) throw err;
-//   fs.mkdir("06-build-page/project-dist/assets", { recursive: true }, (err) => {
-//     if (err) throw err;
-//   });
-//   const dir2 = path.join(__dirname, "project-dist/assets");
-//   files.forEach((el) => {
-//     recursive(el);
-//     function recursive(value) {
-//       if (value.isFile()) {
-//         fs.readFile(`${dir}/${el.name}/${value.name}`, "utf-8", (err, data) => {
-//           if (err) throw err;
-//           else {
-//             fs.writeFile(
-//                 `${dir2}/${el.name}/${value.name}`,
-//                 data,
-//                 "utf-8",
-//                 (err) => {
-//                   if (err) throw err;
-//                 }
-//             );
-//           }
-//         });
-//       } else {
-//         fs.mkdir(
-//             `06-build-page/project-dist/assets/${value.name}`,
-//             { recursive: true },
-//             (err) => {
-//               if (err) throw err;
-//             }
-//         );
+// // fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+// //   if (err) throw err;
+// //   fs.mkdir("06-build-page/project-dist/assets", { recursive: true }, (err) => {
+// //     if (err) throw err;
+// //   });
+// //   const dir2 = path.join(__dirname, "project-dist/assets");
+// //   files.forEach((el) => {
+// //     recursive(el);
+// //     function recursive(value) {
+// //       if (value.isFile()) {
+// //         fs.readFile(`${dir}/${el.name}/${value.name}`, "utf-8", (err, data) => {
+// //           if (err) throw err;
+// //           else {
+// //             fs.writeFile(
+// //                 `${dir2}/${el.name}/${value.name}`,
+// //                 data,
+// //                 "utf-8",
+// //                 (err) => {
+// //                   if (err) throw err;
+// //                 }
+// //             );
+// //           }
+// //         });
+// //       } else {
+// //         fs.mkdir(
+// //             `06-build-page/project-dist/assets/${value.name}`,
+// //             { recursive: true },
+// //             (err) => {
+// //               if (err) throw err;
+// //             }
+// //         );
 
-//         const dir3 = path.join(__dirname, `assets/${value.name}`);
-//         fs.readdir(dir3, { withFileTypes: true }, (err, data) => {
-//           if (err) throw err;
-//           const dir2 = path.join(
-//               __dirname,
-//               `project-dist/assets/${value.name}`
-//           );
-//           data.forEach((elem) => {
-//             recursive(elem);
-//           });
-//         });
-//       }
-//     }
-//   });
-// });
+// //         const dir3 = path.join(__dirname, `assets/${value.name}`);
+// //         fs.readdir(dir3, { withFileTypes: true }, (err, data) => {
+// //           if (err) throw err;
+// //           const dir2 = path.join(
+// //               __dirname,
+// //               `project-dist/assets/${value.name}`
+// //           );
+// //           data.forEach((elem) => {
+// //             recursive(elem);
+// //           });
+// //         });
+// //       }
+// //     }
+// //   });
+// // });
